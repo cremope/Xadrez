f47ec3c0258e31820e2f6904dc78fbfe3fb0ba7b
Ricardo Cremonez <novoa.lopez@gmail.com>
2020-10-13 19:59:30 -0300

    Fazendo os primeiros testes de movimentacao

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,23 +8,50 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
+            try
+            {
+                //PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-            //PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+                //Console.WriteLine(pos);
 
-            //Console.WriteLine(pos);
+                //Console.WriteLine(pos.toPosicao());
+                //Console.ReadLine();
 
-            //Console.WriteLine(pos.toPosicao());
-            //Console.ReadLine();
+                //Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+                //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                //tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 5));
 
-            tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 5));
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-            Tela.ImprimirTabuleiro(tab);
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+
+                }
+
+                
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message); ;
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
