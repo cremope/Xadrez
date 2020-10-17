cdc066fe42ffeb91f8fc098b248c8e92c0ffde0a
Ricardo Cremonez <novoa.lopez@gmail.com>
2020-10-17 16:21:46 -0300

    Controlando as pecas capturadas

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -32,12 +32,7 @@ namespace Xadrez
                     try
                     {
                         Console.Clear();
-                        Tela.ImprimirTabuleiro(partida.tab);
-
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
-
+                        Tela.imprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
